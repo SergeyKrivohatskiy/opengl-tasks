45f5543fc77d1b2d40243d3343a5091ad08148c7
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2016-01-17 18:31:09 +0300

    Empty task2 added
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

---
+++ src/task2/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <gl/glew.h>
+#include <gl/GL.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <AntTweakBar.h>
+#include "../common/shader.hpp"
+
+using namespace glm;
+
+int main(void)
+{
+	return 0;
+}
